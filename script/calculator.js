cc178f1ec2853ea5ca2f75841f40aab623ead22f
johnn600 <johnrey.vilbar@msugensan.edu.ph>
2023-09-08 10:38:58 +0800

    final ver 1

--- script/calculator.js
+++ script/calculator.js
@@ -1,9 +1,8 @@
 function displaySum() {
-    let firstNum = Number(document.getElementById('firstNum').innerHTML)
-    let secondNum = Number(document.getElementById('secondNum').innerHTML)
+    let firstNum = Number(document.getElementById('firstNum').value)
+    let secondNum = Number(document.getElementById('secondNum').value)
   
     let total = firstNum + secondNum;
     document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
   }
   
-  document.getElementById('sumButton').addEventListener("click", displaySum);
